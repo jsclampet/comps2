e9024f35549b5a268cc0b1cb50a672698ceab229
john clampet <jsclampet@gmail.com>
2023-06-03 18:43:48 -0500

    commit for June 3 ... rediscovering module types with a focus on routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,13 +4,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { ElementsModule } from './elements/elements.module';
+import { CollectionsModule } from './collections/collections.module';
+
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ElementsModule,
+    CollectionsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
